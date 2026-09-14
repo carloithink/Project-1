58381de335b5205a28440ebb99eeb458f06da35f
OsvaldoMLZ <osvaldomelendez@my.unt.edu>
2026-09-14 16:39:19 -0500

    Define Node structure in ReservationManager.h
    Added includes and Node structure for reservations.

--- ReservationManager.h
+++ ReservationManager.h
@@ -1,1 +1,17 @@
+#ifndef RESERVATIONMANAGER_H
+#define RESERVATIONMANAGER_H
 
+#include "Resource.h"
+#include "Reservation.h"
+#include <vector>
+#include <queue>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+struct Node {
+  Reservation data;
+  Node* next;
+  Node(const Reservation& res):data9res),next(nullptr) {}
+};
