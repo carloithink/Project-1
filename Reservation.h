2a90de26a50a8ed8acab9736a974ec8e3edff0ed
OsvaldoMLZ <osvaldomelendez@my.unt.edu>
2026-09-14 16:35:48 -0500

    Add Reservation class definition with methods

--- Reservation.h
+++ Reservation.h
@@ -1,1 +1,25 @@
+#ifndef RESERVATION_H
+#define RESERVATION_H
 
+#include <string>
+using namespace std;
+
+class Reservation {
+private:
+  int reservationID;
+  int studentID;
+  string studentName;
+  string resourceID;
+  string date;
+
+public:
+  Reservation();
+  Reservation(int resID, int stuID, string stuName, string resID, string d);
+
+  int getReservationID() const;
+  int getStudentID() const;
+  string getStudentName() const;
+  string getDate() const;
+  void display() const;
+};
+#endif
