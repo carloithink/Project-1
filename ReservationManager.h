89c30c398a4e219c4b405831dbdad1f56a6271f4
OsvaldoMLZ <osvaldomelendez@my.unt.edu>
2026-09-14 17:04:11 -0500

    Fix constructor initialization and update class definition

--- ReservationManager.h
+++ ReservationManager.h
@@ -13,5 +13,41 @@ using namespace std;
 struct Node {
   Reservation data;
   Node* next;
-  Node(const Reservation& res):data9res),next(nullptr) {}
+  Node(const Reservation& res):data(res),next(nullptr) {}
 };
+
+class ReservationManager {
+private:
+  vector<Resource> resources;
+  Node* reservationHead;
+  queue<Reservation> waitlist;
+  stack<Reservation> undoStack
+
+public:
+  ReservationManager();
+  ~ReservationManager();
+
+  void loadResourcesFromFile(const string& fileName);
+  void loadReservationsFromFile(const string& fileName);
+
+  void displayAllResources() const;
+  void displayAvailableResources() const;
+  Resource* findResourcebyID(const string& resID);
+
+  void makeReservation(int resID, int stuID, const string& stuName, const string& sourID, const string& d);
+  bool cancelReservation(int resID);
+  void showActiveReservations() const;
+  Reservation* findReservationbyID(int resID);
+
+  void addToWaitlist(const Reservation& res);
+  void processWaitlist();
+  void showWaitlist() const;
+
+  void undoCancelReservation();
+  void showCancelHistory() const;
+
+  void sortResourcesbyName();
+  void findReservationsbyStudent(int stuID);
+  void generateReports() const;
+};
+#endif
