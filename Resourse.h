6b888cae1f06165f27e833bf9bf334f798705e88
OsvaldoMLZ <osvaldomelendez@my.unt.edu>
2026-09-14 16:46:15 -0500

    Resource Class
    Added Resource class with attributes and methods for handling resource data.

--- Resourse.h
+++ Resourse.h
@@ -1,1 +1,27 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#include <string>
+using namespace std;
+
+class Resource {
+private:
+  string resourceID;
+  string resourceName;
+  string category;
+  string status;
+
+public:
+  Resource();
+  Resource(string ID, string name, string cat, string stat);
+
+  string getResourceID() const;
+  string getResourceName() const;
+  string getCategory() const;
+  string getStatus() const;
+  void setStatus(const string& newStatus);
+  void display() const;
+};
+#endif
+
 
