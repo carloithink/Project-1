94a63477357013f97647b842173486658984cbca
carloithink <145950395+carloithink@users.noreply.github.com>
2026-09-20 21:10:41 -0500

    Refactor Reservation class member names for consistency

--- Reservation.cpp
+++ Reservation.cpp
@@ -5,39 +5,39 @@ C++
 
 // Default Constructor
 Reservation::Reservation() 
-    : reservationId(0), studentId(""), studentName(""), resourceId(""), reservationDate("") {}
+    : reservationID(0), studentID(""), studentName(""), resourceID(""), date("") {}
 
 // Parameterized Constructor
-Reservation::Reservation(int resId, std::string stId, std::string stName, std::string rId, std::string date)
-    : reservationId(resId), studentId(stId), studentName(stName), resourceId(rId), reservationDate(date) {}
+Reservation::Reservation(int resID, std::string stuID, std::string stuName, std::string resID_Param, std::string d)
+    : reservationID(resID), studentID(stuID), studentName(stuName), resourceID(resID_Param), date(d) {}
 
 // Getters
-int Reservation::getReservationId() const {
-    return reservationId;
+int Reservation::getReservationID() const {
+    return reservationID;
 }
 
-std::string Reservation::getStudentId() const {
-    return studentId;
+std::string Reservation::getStudentID() const {
+    return studentID;
 }
 
 std::string Reservation::getStudentName() const {
     return studentName;
 }
 
-std::string Reservation::getResourceId() const {
-    return resourceId;
+std::string Reservation::getResourceID() const {
+    return resourceID;
 }
 
-std::string Reservation::getReservationDate() const {
-    return reservationDate;
+std::string Reservation::getDate() const {
+    return date;
 }
 
-// Display method for a single reservation record
+// Display method
 void Reservation::display() const {
     std::cout << std::left 
-              << std::setw(15) << reservationId
-              << std::setw(15) << studentId
+              << std::setw(15) << reservationID
+              << std::setw(15) << studentID
               << std::setw(22) << studentName
-              << std::setw(15) << resourceId
-              << std::setw(15) << reservationDate << "\n";
+              << std::setw(15) << resourceID
+              << std::setw(15) << date << "\n";
 }
